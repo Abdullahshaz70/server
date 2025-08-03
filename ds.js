bc9729ac547ec2bf14f79b0397a8e535fb7e13ee
AbdullahShahzad-302 <145151091+Abdullahshaz70@users.noreply.github.com>
2025-08-03 17:47:06 +0500

    Update ds.js

--- ds.js
+++ ds.js
@@ -1,12 +1,20 @@
 const {Pool} = require('pg');
 
-const pool = new Pool({
-    user:'postgres',
-    host:'localhost',
-    password:'abdullah7062',
-    database:'chatApp',
-    port: 5432,
+// const pool = new Pool({
+//     user:'postgres',
+//     host:'localhost',
+//     password:'abdullah7062',
+//     database:'chatApp',
+//     port: 5432,
 
+// });
+
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
